75ed901d177cb492a5da1ac0af1f164d04e56375
ZhiLeong <saber_zhileong@hotmail.com>
2023-01-31 17:43:12 -0500

    commit tree ba3979c2d7aa85b983cbd61839800f6798488a4a

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,14 +2,14 @@ class AuthorsController < ApplicationController
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
